8fa5c610ad41d06859d6e801d7806a81d7aae45d
Yoma01 <54567341+Yoma01@users.noreply.github.com>
2025-06-18 09:37:54 -0700

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'projects', 'work', 'publications']
+const section_names = ['home', 'projects', 'work', 'research']
 
 
 window.addEventListener('DOMContentLoaded', event => {
